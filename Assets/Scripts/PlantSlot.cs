2388f48fa90d133325e6e5633814d17d59f0c987
zAllaNz <allanmgoncalves00@gmail.com>
2025-06-02 22:16:03 -0300

    Novo cenário. Implementando as faixas para as plantas

---
+++ Assets/Scripts/PlantSlot.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Plants : MonoBehaviour
+{
+    private Sprite plant_sprite;
+    private GameObject plant_object;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
