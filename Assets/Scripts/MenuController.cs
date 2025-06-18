1fe05ed4a0e74458a29948b6fd9421d41dec6bdf
CodeMateEngineer <inevastrao94@gmail.com>
2025-06-18 17:08:46 -0300

    Novo commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public GameObject settingsPanel;
+
+    public void Jogar() {
+        Debug.Log("Iniciando o jogo");
+        SceneManager.LoadScene("Gameplay");     
+    }
+
+    public void AbrirConfiguracoes() {
+        Debug.Log("Abrindo configuracoes");
+        settingsPanel.SetActive(true);
+    }
+
+    public void FecharConfiguracoes() {
+        Debug.Log("Fechando configuracoes");
+        settingsPanel.SetActive(false);
+    }
+
+    public void SairJogo() {
+        Debug.Log("Jogo fechado.");
+        Application.Quit();
+    }
+}
