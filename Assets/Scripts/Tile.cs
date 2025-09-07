cd88620191dc103a45cdaa55bd9a4385d7be1778
zAllaNz <allanmgoncalves00@gmail.com>
2025-09-06 22:46:20 -0300

    ajustes de conflito

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     private bool isOccupied = false;
-
     private void OnMouseDown()
+    {
+        GameObject plantPrefab = PlantingManager.Instance.GetSelectedPlantPrefab();
+
+        if (plantPrefab != null && !isOccupied)
+        {
+            Instantiate(plantPrefab, transform.position, Quaternion.identity);
+            isOccupied = true;
+            PlantingManager.Instance.PlantWasPlaced();
+            Debug.Log("Planta colocada em " + gameObject.name);
+        }
+        else if (isOccupied)
+        {
+            Debug.Log("Este slot já está ocupado!");
+        }
+        else
+        {
+            Debug.Log("Nenhuma planta selecionada para plantar.");
+        }
+    }
+
+    /*private void OnMouseDown()
     {
         // Pega o prefab do ZombieManager
         GameObject zombiePrefab = ZombieManager.Instance.GetSelectedZombiePrefab();
@@ -27,5 +47,5 @@ public class Tile : MonoBehaviour
             Debug.Log("Nenhum zumbi selecionado para colocar.");
         }
     }
-    // ... resto do script
+    */
 }
